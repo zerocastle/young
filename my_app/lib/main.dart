2ccc395ba36eaecf1eb2c9eceda8e6c00fef5b7d
zerocastle <37466763+zerocastle@users.noreply.github.com>
2021-06-23 16:01:03 +0900

    마더파더 젠틀맨
    마더파더 젠틀멘

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,148 +1,53 @@
 import 'package:flutter/material.dart';
-
 void main() => runApp(MyApp());
 
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var green = Colors.blue;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'gunDabang',
       theme: ThemeData(
-        primarySwatch: Colors.blue
+        primarySwatch: green
       ),
-      home: MyHomePage(),
+      home: MyHomePage(title: '군다방'),
     );
   }
 }
 
+class MyHomePage extends StatelessWidget {
+  final String title;
+  const MyHomePage({ Key? key , required this.title}) : super(key: key);
 
-class MyHomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold (
-      backgroundColor: Colors.amber[800],
-
+    return Scaffold(
       appBar: AppBar(
-        title : Text('gunDabang'),
-        backgroundColor: Colors.amber[700],
-        centerTitle: true,
-        elevation: 0.0,
+        title: Text(this.title),
+        backgroundColor: Colors.green,
       ),
-
-      body: Padding(
-        padding : EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/fire.png'),
-                radius: 60.0,
-              ),
-            ),
-            Divider(
-              height : 60.0,
-              color: Colors.grey[850],
-              thickness: 0.5,
-              endIndent: 30.0,
-            ),
-            Text('NAME' ,
-            style: TextStyle(
-              color : Colors.white,
-              letterSpacing: 2.0,
-              )
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text('GunDabang',
-            style: TextStyle(
-              color : Colors.white,
-              letterSpacing : 2.0,
-              fontSize: 28.0,
-              fontWeight: FontWeight.bold
-            ),
-            ),
-            SizedBox(
-              height : 30.0
-            ),
-            Text('BBanTo Power LEVEL' ,
-            style: TextStyle(
-              color : Colors.white,
-              letterSpacing: 2.0,
-              )
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text('14',
-            style: TextStyle(
-              color : Colors.white,
-              letterSpacing : 2.0,
-              fontSize: 28.0,
-              fontWeight: FontWeight.bold
-            ),
-            ),
-            SizedBox(
-              height : 30.0
-            ),
-            Row(
-              children: <Widget>[
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text('using lightsaber',
-                style : TextStyle(
-                  fontSize: 16.0,
-                  letterSpacing: 1.0
-                )
-                )
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text('fire flames',
-                style : TextStyle(
-                  fontSize: 16.0,
-                  letterSpacing: 1.0
-                )
-                )
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text('머석',
-                style : TextStyle(
-                  fontSize: 16.0,
-                  letterSpacing: 1.0
-                )
-                )
-              ],
-            ),
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/water.png'),
-                radius: 40.0,
-                backgroundColor: Colors.amber[800],
-              ),
-            )
-
-          ],
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            label: '홈',
+            icon: Icon(Icons.home)),
+            BottomNavigationBarItem(
+            label: '쇼핑',
+            icon: Icon(Icons.shopping_bag)),
+            BottomNavigationBarItem(
+            label: '이벤트',
+            icon: Icon(Icons.emoji_events)),
+            BottomNavigationBarItem(
+            label: '게시글',
+            icon: Icon(Icons.keyboard)),
+            BottomNavigationBarItem(
+            label: '마이페이지',
+            icon: Icon(Icons.person))
+        ],
       ),
     );
   }
-
 }
