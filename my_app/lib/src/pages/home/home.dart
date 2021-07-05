e3668110dbdf7a49542605253fdbac993073fc98
kys <romg321@naver.com>
2021-07-06 01:44:44 +0900

    123
    123

--- my_app/lib/src/pages/home/home.dart
+++ my_app/lib/src/pages/home/home.dart
@@ -23,15 +23,17 @@ class _HomeState extends State<Home> {
   void testData() async{
     print('fuck');
     // String url = "http://localhost:3000/login";
-    String url = "http://192.168.15.4:3000/login";
+    // String url = "http://192.168.15.4:3000/login";
+    String url = "http://192.168.15.4:8181/member/getList";
     Network network = await Network(url);
     var data = await network.getJsonData();
     print(data);
+    print(data[0]['email']);
 
-    List<MemberClass> list = [];
-    var size = data.length;
-    var testData = data[0][0];
-    print('size$size data$testData');
+    // List<MemberClass> list = [];
+    // var size = data.length;
+    // var testData = data[0][0];
+    // print('size$size data$testData');
     // for(var i = 0; i < size; i++){
     //   MemberClass member = new MemberClass(mId: data[0][0],mPw: data[0][1]);
     //   list.add(member);
