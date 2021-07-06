d8d1a6616e30f44bbb0ad1e15c7d3f023500344c
kys <romg321@naver.com>
2021-07-07 02:23:22 +0900

    1231
    2321

---
+++ insta_clone2_start-master/insta_clone2_start-master/lib/tab_page.dart
@@ -0,0 +1,57 @@
+import 'package:chapter10/search_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'account_page.dart';
+import 'home_page.dart';
+
+class TabPage extends StatefulWidget {
+//  final FirebaseUser user;
+
+//  TabPage(this.user);
+
+  @override
+  _TabPageState createState() => _TabPageState();
+}
+
+class _TabPageState extends State<TabPage> {
+  int _selectedIndex = 0;
+
+  List _pages;
+
+  @override
+  void initState() {
+    super.initState();
+    _pages = [
+      HomePage(),
+      SearchPage(),
+      AccountPage()
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('tab_page created');
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        fixedColor: Colors.black,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search), title: Text('Search')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle), title: Text('account')),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
