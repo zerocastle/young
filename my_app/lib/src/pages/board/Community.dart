2e43c4961e5595e157f9c1948269ce260b67fc64
kys <romg321@naver.com>
2021-07-19 04:46:51 +0900

    ㅈㅇㄴ 힘들어
    ㅈㅇㄴ 힘들어

--- my_app/lib/src/pages/board/Community.dart
+++ my_app/lib/src/pages/board/Community.dart
@@ -2,6 +2,7 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
 import 'package:my_app/data/network.dart';
 import 'package:my_app/model/BoardVO.dart';
 
@@ -16,14 +17,23 @@ class Community extends StatefulWidget {
 final List<String> imgList = [];
 
 class _CommunityState extends State<Community> {
+  // 게시글
   late Future<dynamic> _boards;
+  // 베스트 게시글
+  late Future<dynamic> _bestBoards;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+
     _boards = boardOrder('/board/getList');
+    _bestBoards = boardOrder('/board/bestList');
+
+    print(_bestBoards.toString());
   }
+
+  // Future<ProcessedData> createData() a
 
   @override
   Widget build(BuildContext context) {
@@ -43,40 +53,8 @@ class _CommunityState extends State<Community> {
                   SizedBox(
                     height: 20.0,
                   ),
-                  Column(
-                    children: [
-                      CarouselSlider(
-                          items: [
-                            Container(
-                              width: 220,
-                              child: Text('bannerCard 1'),
-                              decoration: BoxDecoration(
-                                color: Colors.teal[100],
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10)),
-                              ),
-                            ),
-                            Container(
-                              width: 220,
-                              child: Text('bannerCard 2'),
-                              decoration: BoxDecoration(
-                                color: Colors.teal[100],
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10)),
-                                // border: Border.all(color: Colors.black, width: 3),
-                              ),
-                            ),
-                          ],
-                          options: CarouselOptions(
-                            autoPlay: true,
-                            height: 120,
-                            disableCenter: false,
-                            viewportFraction: 0.6,
-                            autoPlayInterval: const Duration(seconds: 4),
-                            // aspectRatio: 16 / 2,
-                          ))
-                    ],
-                  ),
+                  // Expanded(child : getRankComponent(_bestBoards)),
+                  getRankComponent(_bestBoards),
                   SizedBox(
                     height: 30.0,
                   ),
@@ -100,7 +78,7 @@ class _CommunityState extends State<Community> {
                 ],
               ),
             ),
-            getContent(),
+            // Expanded(child : getRankComponent(_bestBoards)),
             Expanded(child: getDataList(_boards))
           ],
         ),
@@ -109,22 +87,150 @@ class _CommunityState extends State<Community> {
   }
 }
 
+// 게시글 뿌려주는곳
 FutureBuilder getDataList(Future<dynamic> _boards) {
   return FutureBuilder(
     future: _boards,
     builder: (BuildContext context, AsyncSnapshot snapshot) {
-      if (!snapshot.hasData) return CircularProgressIndicator();
+      print(snapshot.data.toString());
+      if (!snapshot.hasData)
+        return Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircularProgressIndicator(),
+          ],
+        ));
       return ListView.builder(
           padding: const EdgeInsets.all(8),
           itemCount: snapshot.data.length,
           itemBuilder: (context, index) {
+            // num number = index + 1;
+            dynamic root = snapshot.data[index];
+            // 타이틀
+            String title = root['btitle'].toString();
+            // 컨텐츠
+            String content = root['bcont'].toString();
+            // 이미지 (파일 이미지 나중에 추가)
+            CircleAvatar image =
+                CircleAvatar(backgroundImage: AssetImage('assets/fire.png'));
+            // 이름
+            String name = root['mid'].toString();
+            // 글 올린 날짜
+            String date = root['bdate'].toString();
+
             return Card(
               child: ListTile(
+                leading: image,
                 onTap: () {},
-                title: Text(snapshot.data[index]['btitle'].toString()),
+                title: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      root['btitle'].toString(),
+                      style:
+                          TextStyle(fontWeight: FontWeight.w700, fontSize: 16),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    RichText(
+                      overflow: TextOverflow.ellipsis,
+                      strutStyle: StrutStyle(fontSize: 12.0),
+                      text: TextSpan(
+                          style: TextStyle(color: Colors.black),
+                          text: root['bcont']),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        // Text('Name | Time '),
+                        Text(name + ' | $date'),
+                        Container(
+                          child: Row(
+                            children: [
+                              Icon(Icons.remove_red_eye_outlined),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text('2'),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Icon(
+                                Icons.favorite,
+                                color: Colors.pink,
+                              ),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text('32'),
+                            ],
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                // leading: ,
               ),
             );
           });
+    },
+  );
+}
+
+// 랭크 보더스
+FutureBuilder getRankComponent(Future<dynamic> _bestBoards) {
+  return FutureBuilder(
+    future: _bestBoards,
+    builder: (BuildContext context, AsyncSnapshot snapshot) {
+      print(snapshot.data.toString());
+      // return Text(snapshot.data.toString());
+      if (!snapshot.hasData)
+        return Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircularProgressIndicator(),
+          ],
+        ));
+
+      return Column(
+        children: [
+          CarouselSlider(
+              items: [
+                Container(
+                  width: 220,
+                  child: Text('bannerCard 1'),
+                  decoration: BoxDecoration(
+                    color: Colors.teal[100],
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                ),
+                Container(
+                  width: 220,
+                  child: Text('bannerCard 2'),
+                  decoration: BoxDecoration(
+                    color: Colors.teal[100],
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    // border: Border.all(color: Colors.black, width: 3),
+                  ),
+                ),
+              ],
+              options: CarouselOptions(
+                autoPlay: true,
+                height: 120,
+                disableCenter: false,
+                viewportFraction: 0.6,
+                autoPlayInterval: const Duration(seconds: 4),
+                // aspectRatio: 16 / 2,
+              ))
+        ],
+      );
     },
   );
 }
@@ -140,65 +246,3 @@ Future<dynamic> boardOrder(String order) async {
   // List<Board> listItem = List<Board>.from(l.map((e) => Board.fromJson(e)));
   return data;
 }
-
-// 컨테츠 함수
-Widget getContent() {
-  return Container(
-    //  color: Colors.redAccent,
-    height: 105,
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Divider(
-          color: Colors.grey[850],
-          thickness: 0.5,
-        ),
-        Text(
-          'Board Title',
-          style: TextStyle(fontWeight: FontWeight.w700, fontSize: 16),
-        ),
-        SizedBox(
-          height: 5,
-        ),
-        Text(
-            'Board Contents Board Contents Board Contents Board Contents Board Contents Board Contents'),
-        // SizedBox(height: 8,),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            // Text('Name | Time '),
-            Text('Name  |  2 Hours ago '),
-            Container(
-              child: Row(
-                children: [
-                  Icon(Icons.remove_red_eye_outlined),
-                  SizedBox(
-                    width: 5,
-                  ),
-                  Text('32'),
-                ],
-              ),
-            ),
-          ],
-        )
-      ],
-    ),
-  );
-}
-
-
-// class bannerCard extends StatefulWidget {
-//   const bannerCard({Key? key}) : super(key: key);
-
-//   @override
-//   _bannerCardState createState() => _bannerCardState();
-// }
-
-// class _bannerCardState extends State<bannerCard> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold();
-//   }
-// }
-
-
