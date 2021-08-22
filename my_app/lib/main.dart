dd9313e51d5c979cbbf0feede5d21e32de097a44
HS_Kang <arkd@gmail.com>
2021-08-22 15:19:52 +0900

    you 답답 머지머지

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_app/root_page.dart';
+// import 'package:my_app/src/widgets/signArmy/DateChoice.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,6 +21,7 @@ class MyApp extends StatelessWidget {
       ),
       // home: MyHomePage(title: '군다방'),
       home: RootPage(),
+      // home: DateChoice(),
     );
   }
   
